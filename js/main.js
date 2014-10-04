e7a8541efc2680bfcce1043ad9f277253f4ce281
Zach Denton <z@chdenton.com>
2014-10-04 15:46:12 -0400

    update site

--- js/main.js
+++ js/main.js
@@ -49,7 +49,7 @@
       this.gl.shaderSource(vertexShader, "attribute vec2 position;\nvoid main(){gl_Position=vec4(position,0,1);}");
       this.gl.compileShader(vertexShader);
       fragmentShader = this.gl.createShader(this.gl.FRAGMENT_SHADER);
-      this.gl.shaderSource(fragmentShader, "precision lowp float;\nuniform float time;uniform vec2 resolution;uniform vec2 mouse;vec3 a(vec3 b){vec4 c=vec4(1.0,2.0/3.0,1.0/3.0,3.0);vec3 d=abs(fract(b.xxx+c.xyz)*6.0-c.www);return b.z*mix(c.xxx,clamp(d-c.xxx,0.0,1.0),b.y);}\n#define N 80\nvoid main(void){vec2 e=(gl_FragCoord.xy-resolution/2.0)/min(resolution.y,resolution.x)*20.0;float f=0.0;float g=3.1415926535*2.0;float h=(-.57166-0.001*mouse.x*0.2+0.0001*time)*g;float i=cos(h);float j=sin(h);vec2 k=vec2(i,-j);vec2 l=vec2(j,i);\nvec2 m=vec2(0,1.0+0.618);float n=1.7171+0.001*mouse.y+0.0001*time;for(int o=0;o<N;o++){float p=dot(e,e);if(p>1.0){p=(1.0)/p;e.x=e.x*p;e.y=e.y*p;}f*=.99;f+=p;e=vec2(dot(e,k),dot(e,l))*n+m;}float q=fract(f);q=2.0*min(q,1.0-q);float r=mod(time*0.025,1.0);float tf=q*sin(0.1*time);gl_FragColor=vec4(a(vec3(r-0.25*q-0.1*abs(tf),1.0 - 0.3*abs(tf),q+0.1*abs(tf))),1.0);}");
+      this.gl.shaderSource(fragmentShader, "precision lowp float;\nuniform float time;uniform vec2 resolution;uniform vec2 mouse;vec3 a(vec3 b){vec4 c=vec4(1.0,2.0/3.0,1.0/3.0,3.0);vec3 d=abs(fract(b.xxx+c.xyz)*6.0-c.www);return b.z*mix(c.xxx,clamp(d-c.xxx,0.0,1.0),b.y);}\n#define N 80\nvoid main(void){vec2 e=((gl_FragCoord.xy-resolution*0.5)/min(resolution.x,resolution.y))*20.0;float f=0.0;float g=3.1415926535*2.0;float h=(-.57166-0.001*mouse.x*0.2+0.0001*time)*g;float i=cos(h);float j=sin(h);vec2 k=vec2(i,-j);vec2 l=vec2(j,i);\nvec2 m=vec2(0,1.0+0.618);float n=1.7171+0.001*mouse.y+0.0001*time;for(int o=0;o<N;o++){float p=dot(e,e);if(p>1.0){p=(1.0)/p;e.x=e.x*p;e.y=e.y*p;}f*=.99;f+=p;e=vec2(dot(e,k),dot(e,l))*n+m;}float q=fract(f);q=2.0*min(q,1.0-q);float r=mod(time*0.025,1.0);float tf=q*sin(0.1*time);gl_FragColor=vec4(a(vec3(r-0.25*q-0.1*abs(tf),1.0 - 0.3*abs(tf),q+0.1*abs(tf))),1.0);}");
       this.gl.compileShader(fragmentShader);
       this.shader = this.gl.createProgram();
       this.gl.attachShader(this.shader, vertexShader);
@@ -80,6 +80,8 @@
       this.devicePixelRatio = (_ref = window.devicePixelRatio) != null ? _ref : 1;
       this.width = this.banner.offsetWidth * this.devicePixelRatio;
       this.height = this.banner.offsetHeight * this.devicePixelRatio;
+      this.canvas.style.width = this.banner.offsetWidth + "px";
+      this.canvas.style.height = this.banner.offsetHeight + "px";
       this.canvas.width = this.width;
       this.canvas.height = this.height;
       this.gl.uniform2f(this.resolutionUniform, this.width, this.height);
@@ -87,7 +89,10 @@
     };
 
     FractalBanner.prototype.mousemove = function(e) {
-      return this.gl.uniform2f(this.mouseUniform, e.clientX / this.width, 1 - e.clientY / this.height);
+      var canvasX, canvasY;
+      canvasX = (e.pageX - this.canvas.offsetLeft) * this.devicePixelRatio;
+      canvasY = (e.pageY - this.canvas.offsetTop) * this.devicePixelRatio;
+      return this.gl.uniform2f(this.mouseUniform, canvasX / this.width, 1 - canvasY / this.height);
     };
 
     FractalBanner.prototype.toggleAnimation = function() {
